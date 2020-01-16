506f0e906024853cac6e1f471e75104eec4d2a22
Goodgo24 <77646307@qq.com>
2020-01-16 23:15:15 +0800

    1

---
+++ jojo-admin/src/main/java/com/joe/jojo/JojoAdminApplication.java
@@ -0,0 +1,13 @@
+package com.joe.jojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JojoAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JojoAdminApplication.class, args);
+    }
+
+}
