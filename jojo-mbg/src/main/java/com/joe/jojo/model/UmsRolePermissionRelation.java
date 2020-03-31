83f16334b3972a8b18e3e57e3357c9053b2927ff
Goodgo24 <77646307@qq.com>
2020-03-31 21:13:33 +0800

    1

---
+++ jojo-mbg/src/main/java/com/joe/jojo/model/UmsRolePermissionRelation.java
@@ -0,0 +1,55 @@
+package com.joe.jojo.model;
+
+import io.swagger.annotations.ApiModelProperty;
+import java.io.Serializable;
+
+public class UmsRolePermissionRelation implements Serializable {
+    @ApiModelProperty(value = "角色-资源关系表主键id")
+    private Long id;
+
+    @ApiModelProperty(value = "角色ID")
+    private Long roleId;
+
+    @ApiModelProperty(value = "资源ID")
+    private Long permission;
+
+    private static final long serialVersionUID = 1L;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public Long getPermission() {
+        return permission;
+    }
+
+    public void setPermission(Long permission) {
+        this.permission = permission;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", roleId=").append(roleId);
+        sb.append(", permission=").append(permission);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+}
